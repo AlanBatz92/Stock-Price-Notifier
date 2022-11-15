c0976e2c0fab34916d50b48b4594ef0d515b7663
dbatz92 <81194130+AlanBatz92@users.noreply.github.com>
2022-11-15 23:45:32 +0000

    Remove inane comments and add more relevant comments

--- main.py
+++ main.py
@@ -6,22 +6,10 @@ import time, os, yagmail
 #add all of these options to a function to keep things organized
 def get_driver():
   options = webdriver.ChromeOptions()
-
-  #website infobars can confuse the cursor when trying to scrape data, so we disable it
   options.add_argument("disable-infobars")
-
-  #starts the browser as maximized, some web pages change the content based on the size
   options.add_argument("start-maximized")
-
-  #avoids potential issues when interacting with a browser on a linux computer
   options.add_argument("disable-dev-shm-usage")
-
-  #browsers, for security, use something called sandboxing.
-  #disabling this gives the scraper more privileges on the page
   options.add_argument("no-sandbox")
-
-  #the following experimental option helps selenium avoid detection by the browser
-  #some pages do not like scripts
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
 
@@ -32,8 +20,8 @@ def get_driver():
 
 
 def clean_text(text):
-  """Extract only the temperature(dynamic value) from text"""
-  #Split the text input using the colon and space delimiter and get the index at 1, convert to a float to be more inclusive
+  """Extract only the number without percent symbol"""
+  #Split the text input using the space delimiter and get the index at 0, convert to a float to be more inclusive
   output = float(text.split(" ")[0])
   return output
 
@@ -50,8 +38,7 @@ def email_notifier(percentage):
 
 def main():
   driver = get_driver()
-  #xpath can be found by inspecting the element you are looking to scrape and right-clicking to copy the xpath
-  #here I am causing the script to sleep for 2 seconds before grabbing the dynamic value from the page, otherwise it will be blank
+
   while True:
     time.sleep(2)
     element = driver.find_element(
